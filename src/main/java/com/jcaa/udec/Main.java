762900f7e048940a44fe7334e1ffaed97a41ccfd
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2026-06-01 22:08:20 -0500

    chore: Logueando el avance del proyecto.

--- src/main/java/com/jcaa/udec/Main.java
+++ src/main/java/com/jcaa/udec/Main.java
@@ -4,9 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
-  private static final String MENSAJE_INICIO = "Capa de Dominio normalizada con DDD y SOLID";
+  private static final String MENSAJE_INICIO_DOMAIN = "Capa de Dominio normalizada con DDD y SOLID";
+  private static final String MENSAJE_INICIO_APPLICATION =
+      "Capa de Aplicacion normalizada con SOLID, DTO Mappers y Casos de uso";
 
   public static void main(String[] args) {
-    log.info(MENSAJE_INICIO);
+    log.info(MENSAJE_INICIO_DOMAIN);
+    log.info(MENSAJE_INICIO_APPLICATION);
   }
 }
