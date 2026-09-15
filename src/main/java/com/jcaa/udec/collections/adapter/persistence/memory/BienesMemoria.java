adf9b187b52c7845760cd73aa2b349bccc15678f
Marlon Paul Perez Herrera <malonpph@gmail.com>
2026-09-14 22:43:09 -0500

    feat: implementar persistencia en memoria de Bien

--- src/main/java/com/jcaa/udec/collections/adapter/persistence/memory/BienesMemoria.java
+++ src/main/java/com/jcaa/udec/collections/adapter/persistence/memory/BienesMemoria.java
@@ -1,4 +1,15 @@
 package com.jcaa.udec.collections.adapter.persistence.memory;
+import com.jcaa.udec.collections.domain.core.model.Bien;
+import java.util.ArrayList;
+import java.util.List;
 
-public class BienesMemoria {
+final class BienesMemoria {
+    private static final List<Bien> BIENES = new ArrayList<>();
+
+    private BienesMemoria() {
+    }
+
+    static List<Bien> obtenerBienes() {
+        return BIENES;
+    }
 }
